580872b347ff044ecbca20b22bb32d2820fc1599
QuyetChien812 <chienvatruong@gmail.com>
2024-10-20 17:00:12 +0700

    update

--- pages/index/main/phone_product.php
+++ pages/index/main/phone_product.php
@@ -1,16 +1,134 @@
-<div class="outstandingPhone"> <!-- mo the outstanding phone-->
-             <div class="outstandingPhone__head_content">
-                <span>Điện thoại nổi bật</span>
-             </div>
-             <div class="outstandingPhone__phone_container">
-                <div class="outstandingPhone__phone_container__phone"> <!-- mo the phone -->
-                     <div class="outstandingPhone__phone_container__phone__phone_content"><span>trả góp 0%</span></div>
-                     <a href="pages/product/outstanding-product-iphone.php"><div class="outstandingPhone__phone_container__phone__phone_image"><img src="imgaes/dt_noiBat/iphone-16-pro-max.webp" alt="" width="100%"></div></a>
-                     <a href="pages/product/outstanding-product-iphone.php"><div class="outstandingPhone__phone_container__phone__phone_name"><span>iPhone 16 Pro Max</span></div></a>
-                     <div class="outstandingPhone__phone_container__phone__phone_price"><span>50.000.000</span></div>
-                </div><!-- dong the phone -->
-                
-                
-             </div>
-        </div>
-        <!-- dong the outstanding phone-->
+<link rel="stylesheet" href="css\phone_product.css">
+<div class="headline">
+<h3>Sản Phẩm Bán Chạy</h3>
+</div>
+<ul class="phone_products">
+   <li>
+      <div class="phone_item">
+         <div class="phone_top">
+            <a href="" class="phone_thumb">
+               <img src="imgaes\dt_noiBat\iphone-16-pro-max.webp" alt="">
+            </a>
+            <a href="" class="detail">Chi tiết</a>
+            <!--------Chi tiết--------->
+         </div>
+         <div class="phone_info">
+            <a href="" class="phone_cat">Iphone</a>
+            <a href="" class="phone_name">Iphone 16 Pro Max</a>
+            <div class="phone_price">19.999.000</div>
+         </div>
+      </div>
+   </li>
+   <li>
+      <div class="phone_item">
+         <div class="phone_top">
+            <a href="" class="phone_thumb">
+               <img src="imgaes\dt_noiBat\samsung-galaxy-s24-plus_1.webp" alt="">
+            </a>
+            <a href="" class="detail">Chi tiết</a>
+            <!--------Chi tiết--------->
+         </div>
+         <div class="phone_info">
+            <a href="" class="phone_cat">Samsung</a>
+            <a href="" class="phone_name">Samsung Galaxy S24 Ultra</a>
+            <div class="phone_price">29.999.000</div>
+         </div>
+      </div>
+   </li>
+   <li>
+      <div class="phone_item">
+         <div class="phone_top">
+            <a href="" class="phone_thumb">
+               <img src="imgaes\dt_noiBat\samsung-galaxy-a05sl.webp" alt="">
+            </a>
+            <a href="" class="detail">Chi tiết</a>
+            <!--------Chi tiết--------->
+         </div>
+         <div class="phone_info">
+            <a href="" class="phone_cat">Samsung</a>
+            <a href="" class="phone_name">Samsung Galaxy A05</a>
+            <div class="phone_price">9.999.000</div>
+         </div>
+      </div>
+   </li>
+   <li>
+      <div class="phone_item">
+         <div class="phone_top">
+            <a href="" class="phone_thumb">
+               <img src="imgaes\dt_noiBat\xiaomi_14t_2_.webp" alt="">
+            </a>
+            <a href="" class="detail">Chi tiết</a>
+            <!--------Chi tiết--------->
+         </div>
+         <div class="phone_info">
+            <a href="" class="phone_cat">Xiaomi</a>
+            <a href="" class="phone_name">Xiaomi 14T</a>
+            <div class="phone_price">19.999.000</div>
+         </div>
+      </div>
+   </li>
+   <li>
+      <div class="phone_item">
+         <div class="phone_top">
+            <a href="" class="phone_thumb">
+               <img src="imgaes\dt_noiBat\xiaomi-redmi-note-13-pro-4g_13__1.webp" alt="">
+            </a>
+            <a href="" class="detail">Chi tiết</a>
+            <!--------Chi tiết--------->
+         </div>
+         <div class="phone_info">
+            <a href="" class="phone_cat">Xiaomi</a>
+            <a href="" class="phone_name">Xiaomi redmi note 13 pro</a>
+            <div class="phone_price">19.999.000</div>
+         </div>
+      </div>
+   </li>
+   <li>
+      <div class="phone_item">
+         <div class="phone_top">
+            <a href="" class="phone_thumb">
+               <img src="imgaes\dt_noiBat\nokia-8210_4g-sand-front_back-int.webp" alt="">
+            </a>
+            <a href="" class="detail">Chi tiết</a>
+            <!--------Chi tiết--------->
+         </div>
+         <div class="phone_info">
+            <a href="" class="phone_cat">Nokia</a>
+            <a href="" class="phone_name">Nokia 8210</a>
+            <div class="phone_price">1.999.000</div>
+         </div>
+      </div>
+   </li>
+   <li>
+      <div class="phone_item">
+         <div class="phone_top">
+            <a href="" class="phone_thumb">
+               <img src="imgaes\dt_noiBat\iphone-16-pro-max.webp" alt="">
+            </a>
+            <a href="" class="detail">Chi tiết</a>
+            <!--------Chi tiết--------->
+         </div>
+         <div class="phone_info">
+            <a href="" class="phone_cat">Iphone</a>
+            <a href="" class="phone_name">Iphone 16 Pro Max</a>
+            <div class="phone_price">19.999.000</div>
+         </div>
+      </div>
+   </li>
+   <li>
+      <div class="phone_item">
+         <div class="phone_top">
+            <a href="" class="phone_thumb">
+               <img src="imgaes\dt_noiBat\iphone-16-pro-max.webp" alt="">
+            </a>
+            <a href="" class="detail">Chi tiết</a>
+            <!--------Chi tiết--------->
+         </div>
+         <div class="phone_info">
+            <a href="" class="phone_cat">Iphone</a>
+            <a href="" class="phone_name">Iphone 16 Pro Max</a>
+            <div class="phone_price">19.999.000</div>
+         </div>
+      </div>
+   </li>
+</ul>
